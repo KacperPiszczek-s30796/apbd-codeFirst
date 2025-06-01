383fd73a109ebc14b87a6b4a4ba7eefca4290fd3
Twoje Imi i Nazwisko <Twj email powizany z GitHubem>
2025-06-01 20:03:41 +0200

    2endpoint complete

--- Services/Service.cs
+++ Services/Service.cs
@@ -69,7 +69,42 @@ public class Service: IService
     public async Task<responseDTO> GetPatientInfo(int patientID, CancellationToken cancellationToken)
     {
         responseDTO responseDTO = new responseDTO();
-        
+        List<Prescription> prescriptions = await prescriptionRepository.GetPrescriptionsByPatientId(patientID, cancellationToken);
+        responseDTO.IdPatient = patientID;
+        Patient patient = prescriptions[0].Patient;
+        responseDTO.FirstName = patient.FirstName;
+        responseDTO.LastName = patient.LastName;
+        responseDTO.Birthdate = patient.BirthDate;
+        List<PrescriptionResponseDTO> prescriptionResponses = new List<PrescriptionResponseDTO>();
+        foreach (var prescription in prescriptions)
+        {
+            PrescriptionResponseDTO prescriptionResponseDTO = new PrescriptionResponseDTO();
+            prescriptionResponseDTO.IdPrescription = prescription.IdPrescription;
+            prescriptionResponseDTO.Date = prescription.Date;
+            prescriptionResponseDTO.DueDate = prescription.DueDate;
+            DoctorResponseDTO doctorResponseDTO = new DoctorResponseDTO();
+            doctorResponseDTO.IdDoctor = prescription.Doctor.IdDoctor;
+            doctorResponseDTO.FirstName = prescription.Doctor.FirstName;
+            doctorResponseDTO.LastName = prescription.Doctor.LastName;
+            doctorResponseDTO.Email = prescription.Doctor.Email;
+            prescriptionResponseDTO.Doctor = doctorResponseDTO;
+            List<MedicamentResponseDTO> medicamentResponses = new List<MedicamentResponseDTO>();
+            List<PrescriptionMedicament> medicaments = await prescriptionRepository.GetPrescriptionMedicamentsByPrescriptionIdAsync(prescription.IdPrescription, cancellationToken);
+            foreach (var medicament in medicaments)
+            {
+                MedicamentResponseDTO medicamentResponseDTO = new MedicamentResponseDTO();
+                medicamentResponseDTO.IdMedicament = medicament.IdMedicament;
+                medicamentResponseDTO.Dose = medicament.Dose;
+                medicamentResponseDTO.Details = medicament.Details;
+                medicamentResponseDTO.Name = medicament.Medicament.Name;
+                medicamentResponseDTO.Description = medicament.Medicament.Description;
+                medicamentResponseDTO.Type = medicament.Medicament.Type;
+                medicamentResponses.Add(medicamentResponseDTO);
+            }
+            prescriptionResponseDTO.Medicaments = medicamentResponses;
+            prescriptionResponses.Add(prescriptionResponseDTO);
+        }
+        responseDTO.Prescriptions = prescriptionResponses;
         return responseDTO;
     }
 }
--- obj/Debug/net8.0/CodeFirstAproach.AssemblyInfo.cs
+++ obj/Debug/net8.0/CodeFirstAproach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeFirstAproach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e93ffe9e5c881803c9d32c1ed37361bb21e6214")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d90b071dee7d59233ece54cd4b8c62d305b89df")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeFirstAproach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeFirstAproach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
